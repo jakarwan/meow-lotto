58b5b01bb310023a8a92a433d7204f4f7efb5a36
jakarwan <jakarwan_159@hotmail.com>
2025-04-09 22:34:59 +0700

    fix url

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 // const API_ENDPOINT = "http://localhost:3035/";
 // const API_ENDPOINT = "https://lottoapi.pg-th.com/";
-const API_ENDPOINT = "https://apidemo.laosspecialvip.com/";
+const API_ENDPOINT = "https://demoapi.laosspecialvip.com/";
 
 export const HTTP = Axios.create({
   baseURL: API_ENDPOINT,
